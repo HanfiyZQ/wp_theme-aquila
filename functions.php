a38df8b2a47f577eba44ef5f8d43e9531b7059f1
Zamaksyary Hanfiy <zamaksyaryhanfiy@gmail.com>
2021-12-23 17:32:22 +0700

    add autoload and trait-singleton

--- functions.php
+++ functions.php
@@ -7,6 +7,17 @@
  */
 
 
+
+if ( ! defined('AQUILA_DIR_PATH')) {
+    define( 'AQUILA_DIR_PATH', untrailingslashit( get_template_directory()) );
+}
+
+require_once AQUILA_DIR_PATH . './inc/helpers/autoloader.php';
+
+function aquila_get_theme_instance() {
+    \AQUILA_THEME\Inc\AQUILA_THEME::get_instance();
+}
+
 function aquila_enqueue_scripts() {
     // Register Styles.
     wp_register_style('style-css', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css'), 'all');
